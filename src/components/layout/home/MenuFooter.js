c53418006740aff2e6570af025be5c1f770a0ce9
Jorge Martinez <estrella161610@gmail.com>
2024-02-10 13:29:27 -0600

    modified

--- src/components/layout/home/MenuFooter.js
+++ src/components/layout/home/MenuFooter.js
@@ -5,22 +5,22 @@ const MenuFooter = () => {
   return (
     <ul className="list-none flex flex-row">
       <li className="mr-4">
-        <Link to="/tecnl/pagina-principal" className="text-menta ">
+        <Link to="#" className="text-menta ">
           Principal
         </Link>
       </li>
       <li className="mr-4">
-        <Link to="/tecnl/eventos" className="text-menta ">
+        <Link to="#" className="text-menta ">
           Menu
         </Link>
       </li>
       <li className="mr-4">
-        <Link to="/tecnl/grupos" className="text-menta ">
+        <Link to="#" className="text-menta ">
           Servicios
         </Link>
       </li>
       <li>
-        <Link to="/tecnl/noticias" className="text-menta ">
+        <Link to="#" className="text-menta ">
           Testimonios
         </Link>
       </li>
